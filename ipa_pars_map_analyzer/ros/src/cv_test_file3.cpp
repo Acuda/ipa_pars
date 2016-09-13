c9a8a29fef2d6ea081bb5091c36ace99cf1f571b
Christian Ehrmann <christianehrmann@gmx.de>
2016-09-13 17:51:53 +0200

    update 160913

--- ipa_pars_map_analyzer/ros/src/cv_test_file3.cpp
+++ ipa_pars_map_analyzer/ros/src/cv_test_file3.cpp
@@ -45,72 +45,128 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "ipa_pars_cv_test_file");
 	std::string image_filename = ros::package::getPath("ipa_pars_map_analyzer") + "/common/files/test_maps/lab_ipa4.png";
 	cv::Mat map = cv::imread(image_filename.c_str(), 0);
-	cv::Mat original_img = map.clone();
-	cv::Mat new_map = map.clone();
-
-	cv::cvtColor(new_map, new_map, CV_GRAY2BGR);
-
-
-	//make non-white pixels black
-	for (int y = 0; y < map.rows; y++)
-	{
-		for (int x = 0; x < map.cols; x++)
-		{
-			//find not reachable regions and make them black
-			if (original_img.at<unsigned char>(y, x) < 250)
-			{
-				original_img.at<unsigned char>(y, x) = 0;
-			}
-			//else make it white
-			else
-			{
-				original_img.at<unsigned char>(y, x) = 255;
-			}
-		}
-	}
+	cv::Mat binary_map = map.clone();
+	cv::threshold( map, binary_map, 250,255, CV_THRESH_BINARY);
 	int erosion_type = cv::MORPH_RECT;
 	int erosion_size = 1;
 	cv::Mat element = getStructuringElement( erosion_type,
 										   cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
 										   cv::Point( erosion_size, erosion_size ) );
-	cv::erode(original_img,new_map,element);
+	cv::Mat erode_map = binary_map.clone();
+	cv::erode(binary_map,erode_map,element);
+	erode_map.convertTo(erode_map, CV_32SC1, 257, 0);
 
-	ROS_INFO("image channels= %u", new_map.channels());
-	cv::Mat new_map_vert = new_map.clone();
-
-
-	cv::Mat output = new_map.clone();
-	cv::Mat input = new_map.clone();
-	unsigned char label = 1;
-//	unsigned char highest_label = 1;
-	int grid_size = 200;
-
-	for (int r = 0; r < input.rows; r+= grid_size)
+//	cv::Rect roi_square = cv::Rect(100,100,100,100);
+//	cv::Mat map_roi = erode_map(roi_square);
+//	cv::Mat mask;
+//	cv::inRange(map_roi, 65535, 65535, mask);
+//	map_roi.setTo(40000, mask);
+//	cv::imshow("map_roi", map_roi);
+	ROS_INFO("Image rows x cols = %u x %u", erode_map.rows, erode_map.cols);
+	int grid_size = 100;
+	ROS_INFO("grid_size = %u", grid_size);
+	for (int r = 0; r < erode_map.rows; r+=grid_size)
 	{
-		ROS_INFO("r = %u", r);
-		for (int c = 0; c < input.cols; c+= grid_size)
+		for (int c = 0; c < erode_map.cols; c+=grid_size)
 		{
-			int endc = grid_size;
-			int endr = grid_size;
-			if (r+grid_size > input.rows)
+			int width = grid_size;
+			int hight = grid_size;
+
+			if (c+grid_size >= erode_map.cols)
 			{
-				endr = input.rows-1 - r;
+				width = erode_map.cols-1 - c;
 			}
-			if (c+grid_size > input.cols)
+			if (r+grid_size >= erode_map.rows)
 			{
-				endc = input.cols-1 - c;
+				hight = erode_map.rows-1 -r;
 			}
-			cv::Rect roi_square = cv::Rect(c,r, endc, endr);
-			cv::Mat image_roi = input(roi_square);
-			image_roi.setTo(label);
+
+			// select square as region of interest (roi)
+			cv::Rect roi_square = cv::Rect(c,r,width,hight);
+			cv::Mat map_roi = erode_map(roi_square);
+//			cv::Mat map_roi_copy = map_roi.clone();
+
+			//todo make borders around OR think of not checking the first lines of the image?!?
+			cv::Mat map_roi_copy(map_roi.rows+2, map_roi.cols+2, map_roi);
+			cv::copyMakeBorder( map_roi, map_roi_copy, 2, 2, 2, 2, cv::BORDER_CONSTANT, 0 );
+			cv::imshow("map_roi_copy",map_roi_copy);
 //			cv::Mat mask;
-//			cv::Rect rect;
-//			mask.create(image_roi.rows+2, image_roi.cols+2, CV_8UC1);
-//			cv::floodFill(image_roi, mask, cv::Point(0,0) , cv::Scalar(label), &rect, cv::Scalar(0),
-//					cv::Scalar(0), 4);
-			label++;
+//			cv::inRange(map_roi, 65535, 65535, mask);
+//			map_roi.setTo(label, mask);
+			int label = 0;
+			std::map<int,int> lookup_label_map;
+			for (int x = 0; x < map_roi.rows; x++)
+			{
+				label++;
+				for (int y = 0; y < map_roi.cols; y++)
+				{
+					// label left
+					if (map_roi.at<int>(y-1,x) == 0)
+					{
+						label++;
+					}
+					map_roi_copy.at<int>(y,x) = label;
+
+					//label above
+					if (map_roi.at<int>(y,x-1) != 0 && map_roi.at<int>(y-1,x) < label)
+					{
+						// if label doesnt exist in map
+						if (lookup_label_map.find(label) != lookup_label_map.end())
+						{
+							lookup_label_map.at(label) = map_roi.at<int>(y-1,x);
+						}
+
+					}
+				}
+			}
 		}
 	}
+
+	cv::Mat output = erode_map.clone();
+
+//	int erosion_type = cv::MORPH_RECT;
+//	int erosion_size = 1;
+//	cv::Mat element = getStructuringElement( erosion_type,
+//										   cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+//										   cv::Point( erosion_size, erosion_size ) );
+//	cv::erode(original_img,new_map,element);
+//
+//	ROS_INFO("image channels= %u", new_map.channels());
+//	cv::Mat new_map_vert = new_map.clone();
+//
+//
+//	cv::Mat output = new_map.clone();
+//	cv::Mat input = new_map.clone();
+//	unsigned char label = 100;
+////	unsigned char highest_label = 1;
+//	int grid_size = 200;
+//
+//	for (int r = 0; r < input.rows; r+= grid_size)
+//	{
+//		ROS_INFO("r = %u", r);
+//		for (int c = 0; c < input.cols; c+= grid_size)
+//		{
+//			int endc = grid_size;
+//			int endr = grid_size;
+//			if (r+grid_size > input.rows)
+//			{
+//				endr = input.rows-1 - r;
+//			}
+//			if (c+grid_size > input.cols)
+//			{
+//				endc = input.cols-1 - c;
+//			}
+//			cv::Rect roi_square = cv::Rect(c,r, endc, endr);
+//			cv::Mat image_roi = input(roi_square);
+//			output.setTo(label, output);
+////			cv::Mat mask;
+////			cv::Rect rect;
+////			mask.create(image_roi.rows+2, image_roi.cols+2, CV_8UC1);
+////			cv::floodFill(image_roi, mask, cv::Point(0,0) , cv::Scalar(label), &rect, cv::Scalar(0),
+////					cv::Scalar(0), 4);
+//			label++;
+//		}
+//	}
 
 
 //	int factor_x = floor(input.cols / grid_size);
@@ -188,26 +244,26 @@ int main(int argc, char **argv)
 //		label = highest_label;
 //	}
 
-	std::vector<int> list_of_labels;
-	for (int r = 0; r < output.rows; ++r)
-	{
-		for ( int c = 0; c < output.cols; ++c)
-		{
-			if (output.at<int>(r,c) != 0)
-			{
-				int label = output.at<int>(r,c);
-				if (std::find(list_of_labels.begin(), list_of_labels.end(), label) != list_of_labels.end())
-				{
-
-				}
-				else
-				{
-			//		ROS_INFO_STREAM("Adding label =" << label);
-					list_of_labels.push_back(label);
-				}
-			}
-		}
-	}
+//	std::vector<int> list_of_labels;
+//	for (int r = 0; r < output.rows; ++r)
+//	{
+//		for ( int c = 0; c < output.cols; ++c)
+//		{
+//			if (output.at<int>(r,c) != 0)
+//			{
+//				int label = output.at<int>(r,c);
+//				if (std::find(list_of_labels.begin(), list_of_labels.end(), label) != list_of_labels.end())
+//				{
+//
+//				}
+//				else
+//				{
+//			//		ROS_INFO_STREAM("Adding label =" << label);
+//					list_of_labels.push_back(label);
+//				}
+//			}
+//		}
+//	}
 
 //	unsigned char first_label_in_row = 1;
 //	unsigned char last_label_in_row = 0;
@@ -278,33 +334,33 @@ int main(int argc, char **argv)
 //		countery++;
 //	}
 
-	//display
-	cv::Mat colour_map = new_map.clone();
-	cv::cvtColor(colour_map, colour_map, CV_GRAY2BGR);
-	ROS_INFO_STREAM("list_of_labels.size() "<< list_of_labels.size());
-	for (int l = 0; l < list_of_labels.size(); l++)
-	{
-		//choose random color for each room
-		int blue = (rand() % 250) + 1;
-		int green = (rand() % 250) + 1;
-		int red = (rand() % 250) + 1;
-		for (int y = 0; y < output.rows; y++)
-		{
-			for (int x = 0; x < output.cols; x++)
-			{
-				if (output.at<unsigned char>(y,x) ==  list_of_labels.at(l))
-				{
-					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(blue,green,red);
-				}
-				else if (output.at<unsigned char>(y,x) == 250)
-				{
-					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(255,255,255);
-				}
-			}
-		}
-	}
-
-	cv::imshow("testoutput_color", colour_map);
+//	//display
+//	cv::Mat colour_map = new_map.clone();
+//	cv::cvtColor(colour_map, colour_map, CV_GRAY2BGR);
+//	ROS_INFO_STREAM("list_of_labels.size() "<< list_of_labels.size());
+//	for (int l = 0; l < list_of_labels.size(); l++)
+//	{
+//		//choose random color for each room
+//		int blue = (rand() % 250) + 1;
+//		int green = (rand() % 250) + 1;
+//		int red = (rand() % 250) + 1;
+//		for (int y = 0; y < output.rows; y++)
+//		{
+//			for (int x = 0; x < output.cols; x++)
+//			{
+//				if (output.at<unsigned char>(y,x) ==  list_of_labels.at(l))
+//				{
+//					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(blue,green,red);
+//				}
+//				else if (output.at<unsigned char>(y,x) == 250)
+//				{
+//					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(255,255,255);
+//				}
+//			}
+//		}
+//	}
+//
+//	cv::imshow("testoutput_color", colour_map);
 
 
 //	//same vertical
@@ -385,33 +441,64 @@ int main(int argc, char **argv)
 //
 //
 ////	cv::imshow("testoutput_map", new_map);
-//
-//	//display
-//	cv::Mat colour_map_vert = new_map_vert.clone();
-//	cv::cvtColor(colour_map_vert, colour_map_vert, CV_GRAY2BGR);
-//	ROS_INFO_STREAM("list_of_labels.size() "<< list_of_labels_vert.size());
-//	for (int l = 0; l < list_of_labels_vert.size(); l++)
-//	{
-//		//choose random color for each room
-//		int blue = (rand() % 250) + 1;
-//		int green = (rand() % 250) + 1;
-//		int red = (rand() % 250) + 1;
-//		for (int y = 0; y < new_map_vert.rows; y++)
-//		{
-//			for (int x = 0; x < new_map_vert.cols; x++)
-//			{
-//				if (new_map_vert.at<unsigned char>(y,x) ==  list_of_labels_vert.at(l))
-//				{
-//					colour_map_vert.at<cv::Vec3b>(y,x) = cv::Vec3b(blue,green,red);
-//				}
-//				else if (new_map_vert.at<unsigned char>(y,x) == 250)
-//				{
-//					colour_map_vert.at<cv::Vec3b>(y,x) = cv::Vec3b(255,255,255);
-//				}
-//			}
-//		}
-//	}
-//	cv::imshow("testoutput_color_vert", colour_map_vert);
+
+	// find list of labels:
+	std::vector<int> list_of_labels;
+	for (int r = 0; r<output.rows; r++)
+	{
+		for (int c = 0; c<output.cols; c++)
+		{
+			int label = output.at<int>(r,c);
+			if (std::find(list_of_labels.begin(), list_of_labels.end(), label) != list_of_labels.end())
+			{
+
+			}
+			else
+			{
+				list_of_labels.push_back(label);
+			}
+		}
+	}
+
+	//display list of labels:
+	for (int i = 0; i<list_of_labels.size(); i++)
+	{
+		ROS_INFO("labels are = %d ", list_of_labels.at(i));
+	}
+
+	//display
+	cv::Mat colour_map = output.clone();
+	colour_map.convertTo(colour_map, CV_8U);
+	cv::cvtColor(colour_map, colour_map, CV_GRAY2BGR);
+	ROS_INFO_STREAM("list_of_labels.size() "<< list_of_labels.size());
+	for (int l = 0; l < list_of_labels.size(); l++)
+	{
+		//choose random color for each room
+		int blue = (rand() % 250) + 1;
+		int green = (rand() % 250) + 1;
+		int red = (rand() % 250) + 1;
+
+		for (int y = 0; y < output.rows; y++)
+		{
+			for (int x = 0; x < output.cols; x++)
+			{
+				if (output.at<int>(y,x) == 0)
+				{
+					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(0,0,0);
+				}
+				else if (output.at<int>(y,x) == 65535)
+				{
+					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(255,255,255);
+				}
+				else if (output.at<int>(y,x) ==  list_of_labels.at(l))
+				{
+					colour_map.at<cv::Vec3b>(y,x) = cv::Vec3b(blue,green,red);
+				}
+
+			}
+		}
+	}
+	cv::imshow("testoutput_color", colour_map);
 	cv::waitKey();
 	//exit
 	return 0;
